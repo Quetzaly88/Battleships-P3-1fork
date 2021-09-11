3b6625982a81a59d6e2854a182c72a6fc76f6850
mmoore483 <mary.e.moore@hotmail.co.uk>
2021-09-11 11:49:20 +0000

    Initial commit and make the battleships board

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+board = []
+
+
+def make_board():
+    for ind in range(0,5):
+        board.append(" X "*5)
+    return board
+
+
+def print_board(board):
+    for ind in board:
+        print(ind)
+
+
+make_board()
+print_board(board)
