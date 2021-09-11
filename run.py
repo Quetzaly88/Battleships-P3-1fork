d9fd0d8fc5b8a052941886a377c1ce8680d13dad
mmoore483 <mary.e.moore@hotmail.co.uk>
2021-09-11 12:05:41 +0000

    Add function to generate random number and random ship location

--- run.py
+++ run.py
@@ -1,19 +1,49 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint 
+
+
 board = []
 
 
 def make_board():
-    for ind in range(0,5):
+    """
+    Make the starting board of X, it has no required arguments and returns the
+    board
+    """
+
+    for ind in range(0, 5):
         board.append(" X "*5)
     return board
 
 
 def print_board(board):
+    """
+    prints the board
+    """
     for ind in board:
         print(ind)
 
 
-make_board()
-print_board(board)
+def random_num(board):
+    """
+    Generate a random number between 0 and the length of the board minus one.
+    We minus 1 because len starts at 1 but the board and arrays etc. start at 0
+    """
+    return randint(0, len(board)-1) 
+
+
+def generate_ship_loc(board):
+    ship_col = random_num(board)
+    ship_row = random_num(board)
+    print(ship_col, ship_row)
+
+
+def main():
+    make_board()
+    print_board(board)
+    generate_ship_loc(board)
+
+main()
+
